904013f2d37eb70b397ea8a630282063ff15507b
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-07-13 17:09:55 +1000

    Project Initialized

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import UserComponent from "./components/UserComponent";
 import "./App.css";
 
 const App = () => {
-    return (
-        <div className="container">
-            <h1>Hello, Welcome to React and TypeScript</h1>
-            <FirstComponent />
-            <UserComponent
-                name="Peter Parker"
-                age={30}
-                address="Brooklyn, NY 1000"
-                dob={new Date()}
-            />
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>Hello, Welcome to React and TypeScript</h1>
+      <FirstComponent />
+      <UserComponent
+        name="Bikal Adhikari"
+        age={30}
+        address="Sydney, Australia"
+        dob={new Date()}
+      />
+    </div>
+  );
 };
 
 export default App;
